679c49dd609e118c782c4c01aa067f30811c4fe4
rutikparmar445 <rutikparmar445@gmail.com>
2024-10-02 14:30:59 +0530

    update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import './App.css'
-// import NavBar from './components/NavBar'
-// import Home from './components/Home'
-// import WatchList from './components/WatchList'
-// import { Routes, Route } from 'react-router-dom'
-// import WatchListContextWrapper from './context/WatchListContext'
+import NavBar from './components/NavBar'
+import Home from './components/Home'
+import WatchList from './components/WatchList'
+import { Routes, Route } from 'react-router-dom'
+import WatchListContextWrapper from './context/WatchListContext'
 // import User from './components/User'
-import UserRedux from './components/UserRedux'
+// import UserRedux from './components/UserRedux'
 
 function App() {
 
   return (
     <>
       {/* <User /> */}
-      <UserRedux />
-      {/* <NavBar />
+      {/* <UserRedux /> */}
+      <NavBar />
       <WatchListContextWrapper>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/watchlist" element={<WatchList />} />
         </Routes> 
-      </WatchListContextWrapper> */}
+      </WatchListContextWrapper>
     </>
   )
 }
